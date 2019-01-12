3a624c9922b5ff4bdcec66baa4b34f69b196d73b
Ming Ming <mint2648@gmail.com>
2019-01-12 23:57:21 +0800

    Move utils into its own file

--- src/app/script/create_sqlite_db.py
+++ src/app/script/create_sqlite_db.py
@@ -4,3 +4,6 @@ import app.model as model
 def create_sqlite_db():
 	sqlite_engine = create_engine("sqlite:///poll.db")
 	model.Base.metadata.create_all(sqlite_engine)
+
+if __name__ == "__main__":
+	create_sqlite_db()
